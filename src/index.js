f550912c2563e5b3be857455a84fef6853db6433
sharon-leshan <maleos380@gmail.com>
2022-06-14 17:10:36 +0300

    prime solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 // complete the function
 function prime (num) {
   // code goes here
+  if(num === 2 ) return true
+  for(let i = 2; i < num; i++){
+    if(num % i === 0){
+      return false
+    }
+  }
+  return true
 }
 
 function solution (arg) {
